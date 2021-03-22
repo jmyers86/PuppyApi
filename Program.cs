20a7773c4e5e1ca135b9277ad68bd328260600dd
Joe Myers <joecaricature@gmail.com>
2021-03-22 19:06:27 -0400

    Beginning Http Client

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace PuppyApi
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            // Random image
+            var responseAsString = await client.GetStringAsync("https://dog.ceo/api/breeds/image/random");
+
+            Console.WriteLine(responseAsString);
+        }
     }
-  }
 }
