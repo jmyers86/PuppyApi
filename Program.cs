db4e1adede765003132bd25ac494dee84c9221eb
Joe Myers <joecaricature@gmail.com>
2021-03-24 20:16:01 -0400

    Changed api from dogs to breweries

--- Program.cs
+++ Program.cs
@@ -8,23 +8,37 @@ namespace PuppyApi
 {
     class Program
     {
-        class Puppy
+        class Brewery
         {
-            public Array message { get; set; }
-            public bool status { get; set; }
+            public string name { get; set; }
+            public string brewery_type { get; set; }
+            public string street { get; set; }
+            public string address_2 { get; set; }
+            public string address_3 { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+            public string county_province { get; set; }
+            public string postal_code { get; set; }
+            public string country { get; set; }
+            public string longitude { get; set; }
+            public string latitude { get; set; }
+            public string phone { get; set; }
+            public string website_url { get; set; }
+            public DateTime updated_at { get; set; }
+            public DateTime created_at { get; set; }
         }
         static async Task Main(string[] args)
         {
             var client = new HttpClient();
 
             // All dog breeds
-            var responseAsStream = await client.GetStreamAsync("https://dog.ceo/api/breeds/list/all");
+            var responseAsStream = await client.GetStreamAsync("https://api.openbrewerydb.org/breweries");
 
-            var breeds = await JsonSerializer.DeserializeAsync<List<Puppy>>(responseAsStream);
+            var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseAsStream);
 
-            foreach (var breed in breeds)
+            foreach (var brewery in breweries)
             {
-                Console.WriteLine(breed.message);
+                Console.WriteLine($"{brewery.name}, is a {brewery.brewery_type} brewery. It is located in {brewery.city}, {brewery.state}");
             }
             // Console.WriteLine(responseAsStream);
         }
