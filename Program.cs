960562e247687f960c4852417541bf41bcbe592a
Joe Myers <joecaricature@gmail.com>
2021-03-25 20:14:46 -0400

    Finishing touches on the Menu

--- Program.cs
+++ Program.cs
@@ -16,8 +16,9 @@ namespace PuppyApi
             Console.WriteLine();
             Console.WriteLine("Welcome to the Ultimate Brewery Directory");
             Console.WriteLine();
-            Console.WriteLine("Below are the first 20 entries in our list:");
+            Console.WriteLine("{message}:");
         }
+
         class Brewery
         {
             [JsonPropertyName("name")]
@@ -55,68 +56,93 @@ namespace PuppyApi
         }
         static async Task Main(string[] args)
         {
-
-        }
-        static async Task BreweriesByState()
-        {
-            var client = new HttpClient();
-
-            // First 20 breweries in list -by State.
-            var responseAsStream = await client.GetStreamAsync("https://api.openbrewerydb.org/breweries");
-
-            var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseAsStream);
-
-            var table = new ConsoleTable("Name", "Brewery Type", "City", "State", "Country", "Created On", "Updated On");
-
-            foreach (var brewery in breweries)
+            var isRunning = true;
+            while (isRunning)
             {
-                table.AddRow(brewery.Name, brewery.BreweryType, brewery.City, brewery.State, brewery.Country, brewery.CreatedAt, brewery.UpdatedAt);
+                MenuGreeting("``~*.*-------*.*~``");
+                var selection = MenuPrompt("Choose an option below:");
+                switch (selection)
+                {
+                    case 0:
+                        Console.WriteLine("Goodbye!");
+                        isRunning = false;
+                        break;
+
+                    case 1:
+                        await BreweriesByState();
+                        break;
+
+                    case 2:
+                        await BreweriesInTampa();
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry, please choose a valid option.");
+                        break;
+
+                }
+            }
+            static async Task BreweriesByState()
+            {
+                var client = new HttpClient();
+
+                // First 20 breweries in list -by State.
+                var responseAsStream = await client.GetStreamAsync("https://api.openbrewerydb.org/breweries");
+
+                var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseAsStream);
+
+                var table = new ConsoleTable("Name", "Brewery Type", "City", "State", "Country", "Created On", "Updated On");
+
+                foreach (var brewery in breweries)
+                {
+                    table.AddRow(brewery.Name, brewery.BreweryType, brewery.City, brewery.State, brewery.Country, brewery.CreatedAt, brewery.UpdatedAt);
+                }
+
+                table.Write();
             }
 
-            table.Write();
-        }
-
-        static async Task BreweriesByCity()
-        {
-            var client = new HttpClient();
-
-            // First 20 breweries in Tampa.
-            var responseAsStream = await client.GetStreamAsync("https://api.openbrewerydb.org/breweries?by_city=tampa");
-
-            var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseAsStream);
-
-            var table = new ConsoleTable("Name", "Brewery Type", "City", "State", "Country", "Created On", "Updated On");
-
-            foreach (var brewery in breweries)
+            static async Task BreweriesInTampa()
             {
-                table.AddRow(brewery.Name, brewery.BreweryType, brewery.City, brewery.State, brewery.Country, brewery.CreatedAt, brewery.UpdatedAt);
+                var client = new HttpClient();
+
+                // First 20 breweries in Tampa.
+                var responseAsStream = await client.GetStreamAsync("https://api.openbrewerydb.org/breweries?by_city=tampa");
+
+                var breweries = await JsonSerializer.DeserializeAsync<List<Brewery>>(responseAsStream);
+
+                var table = new ConsoleTable("Name", "Brewery Type", "City", "State", "Country", "Created On", "Updated On");
+
+                foreach (var brewery in breweries)
+                {
+                    table.AddRow(brewery.Name, brewery.BreweryType, brewery.City, brewery.State, brewery.Country, brewery.CreatedAt, brewery.UpdatedAt);
+                }
+
+                table.Write();
             }
 
-            table.Write();
-        }
-
-
-        static int MenuPrompt(string prompt)
-        {
-            Console.WriteLine(prompt);
-            Console.WriteLine("1) List Breweries by State (Ascending)");
-            Console.WriteLine("2) List Breweries in Tampa (Ascending)");
-            Console.WriteLine("Type '0' to exit the program");
-            string input;
-            int value;
-            do
+            static int MenuPrompt(string prompt)
             {
-                Console.Write(">");
-                input = Console.ReadLine();
-            } while (!int.TryParse(input, out value));
-            return value;
-        }
-        static string PromptForString(string prompt)
-        {
-            Console.WriteLine();
-            Console.WriteLine(prompt);
-            Console.Write("> ");
-            return Console.ReadLine();
+                Console.WriteLine(prompt);
+                Console.WriteLine("1) List Breweries by State (Ascending)");
+                Console.WriteLine("2) List Breweries in Tampa (Ascending)");
+                Console.WriteLine("Type '0' to exit the program");
+                string input;
+                int value;
+                do
+                {
+                    Console.Write(">");
+                    input = Console.ReadLine();
+                } while (!int.TryParse(input, out value));
+                return value;
+            }
+
+            // static string PromptForString(string prompt)
+            // {
+            //     Console.WriteLine();
+            //     Console.WriteLine(prompt);
+            //     Console.Write("> ");
+            //     return Console.ReadLine();
+            // }
         }
     }
 }
